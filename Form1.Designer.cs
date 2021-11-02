2f281bf505592f12e9de7a182bdcfb5053fe5f5f
agnawoussama@gmail.com <agnawoussama@gmail.com>
2021-11-02 15:25:35 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace TP05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.cmbxChercher = new System.Windows.Forms.ComboBox();
+            this.btnChercher = new System.Windows.Forms.Button();
+            this.txtbxChercher = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtbxChercher);
+            this.groupBox1.Controls.Add(this.btnChercher);
+            this.groupBox1.Controls.Add(this.cmbxChercher);
+            this.groupBox1.Font = new System.Drawing.Font("Source Sans Pro", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(497, 61);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Recherche Avancée";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 79);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(497, 260);
+            this.dgv.TabIndex = 1;
+            // 
+            // cmbxChercher
+            // 
+            this.cmbxChercher.FormattingEnabled = true;
+            this.cmbxChercher.Location = new System.Drawing.Point(14, 24);
+            this.cmbxChercher.Name = "cmbxChercher";
+            this.cmbxChercher.Size = new System.Drawing.Size(180, 28);
+            this.cmbxChercher.TabIndex = 0;
+            // 
+            // btnChercher
+            // 
+            this.btnChercher.Location = new System.Drawing.Point(362, 22);
+            this.btnChercher.Name = "btnChercher";
+            this.btnChercher.Size = new System.Drawing.Size(117, 31);
+            this.btnChercher.TabIndex = 1;
+            this.btnChercher.Text = "Chercher";
+            this.btnChercher.UseVisualStyleBackColor = true;
+            this.btnChercher.Click += new System.EventHandler(this.btnChercher_Click);
+            // 
+            // txtbxChercher
+            // 
+            this.txtbxChercher.Location = new System.Drawing.Point(230, 24);
+            this.txtbxChercher.Name = "txtbxChercher";
+            this.txtbxChercher.Size = new System.Drawing.Size(117, 28);
+            this.txtbxChercher.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 351);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "TP 05";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtbxChercher;
+        private System.Windows.Forms.Button btnChercher;
+        private System.Windows.Forms.ComboBox cmbxChercher;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace TP05
+{
+    public partial class Form1 : Form
+    {
+        static string connexionString = ConfigurationManager.ConnectionStrings["conString"].ToString();
+        SqlConnection con = new SqlConnection(connexionString);
+        BindingSource bs = new BindingSource();
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] comboArray = { "Tout", "Commencent par", "Ne Commencent pas par", "Se terminent par", "Ne Se terminent pas par", "Contiennent", "Ne contiennent pas" };
+            cmbxChercher.Items.AddRange(comboArray);
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void btnChercher_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+            void execute()
+            {
+                con.Open();
+                dr = cmd.ExecuteReader();
+                bs.DataSource = dr;
+                dgv.DataSource = bs;
+            }
+            switch (cmbxChercher.SelectedIndex)
+            {
+                //Tout
+                case 0:
+                    try
+                    {
+                        cmd.CommandText = "SELECT * FROM Client";
+                        execute();
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                    finally { con.Close(); }        
+                    break;
+                //Commencent par
+                case 1:
+                    try
+                    {
+                        cmd.CommandText = "SELECT * FROM Client WHERE nomClient LIKE @nom";
+                        cmd.Parameters.AddWithValue("@nom", txtbxChercher.Text +"%");
+                        execute();
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                    finally { con.Close(); }
+                    break;
+                //Ne Commencent pas par
+                case 2:
+                    try
+                    {
+                        cmd.CommandText = "SELECT * FROM Client WHERE nomClient NOT LIKE @nom";
+                        cmd.Parameters.AddWithValue("@nom", txtbxChercher.Text + "%");
+                        execute();
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                    finally { con.Close(); }
+                    break;
+                //Se terminent par
+                case 3:
+                    try
+                    {
+                        cmd.CommandText = "SELECT * FROM Client WHERE nomClient LIKE @nom";
+                        cmd.Parameters.AddWithValue("@nom", "%"+txtbxChercher.Text);
+                        execute();
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                    finally { con.Close(); }
+                    break;
+                //Ne Se terminent pas par
+                case 4:
+                    try
+                    {
+                        cmd.CommandText = "SELECT * FROM Client WHERE nomClient NOT LIKE @nom";
+                        cmd.Parameters.AddWithValue("@nom", "%" + txtbxChercher.Text);
+                        execute();
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                    finally { con.Close(); }
+                    break;
+                //Contiennent
+                case 5:
+                    try
+                    {
+                        cmd.CommandText = "SELECT * FROM Client WHERE nomClient LIKE @nom";
+                        cmd.Parameters.AddWithValue("@nom", "%" +txtbxChercher.Text+ "%");
+                        execute();
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                    finally { con.Close(); }
+                    break;
+                //Ne contiennent pas
+                case 6:
+                    try
+                    {
+                        cmd.CommandText = "SELECT * FROM Client WHERE nomClient NOT LIKE @nom";
+                        cmd.Parameters.AddWithValue("@nom", "%" + txtbxChercher.Text + "%");
+                        execute();
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                    finally { con.Close(); }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
